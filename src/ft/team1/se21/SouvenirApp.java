5851c33aa6c286c5c1fb55339ae5275941307500
ashok <ashokanurudh@ymail.com>
2013-03-15 18:46:58 +0800

    updated ashok

--- src/ft/team1/se21/SouvenirApp.java
+++ src/ft/team1/se21/SouvenirApp.java
@@ -8,7 +8,7 @@ public class SouvenirApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Edited by Nav");
-		System.out.println("edited by ashok1");
+		System.out.println("edited by ashok12");
 		
 		
 
