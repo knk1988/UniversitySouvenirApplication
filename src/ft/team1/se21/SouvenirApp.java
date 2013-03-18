14f3745c72096106c3e23756d372c05ad5d210e1
choujerry <chouying.taiwan@gmail.com>
2013-03-18 12:27:56 +0800

    add "local"

--- src/ft/team1/se21/SouvenirApp.java
+++ src/ft/team1/se21/SouvenirApp.java
@@ -8,7 +8,7 @@ public class SouvenirApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Code of the main method goes here!");
-		System.out.println("haha");
+		System.out.println("local");
 		
 		
 
