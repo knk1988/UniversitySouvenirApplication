36b21086ab7a2c1f11948eb03cb010888e710303
rinu sharma <rinusharma157@gmail.com>
2013-03-20 16:53:05 +0800

    checking in Discount_offersDataFile with updated code

--- src/ft/team1/se21/datafile/Discount_OffersDataFile.java
+++ src/ft/team1/se21/datafile/Discount_OffersDataFile.java
@@ -6,36 +6,70 @@ import java.util.List;
 
 import ft.team1.se21.constants.Constants;
 import ft.team1.se21.model.DiscountAndOffers;
-import ft.team1.se21.model.Vendor;
+
 
 public class Discount_OffersDataFile extends DataFile {
-	List<DiscountAndOffers> discountOffersList;
-	List<Vendor> vendorList;
+	private static Discount_OffersDataFile discount_OffersDataFile;
 
-	public List<DiscountAndOffers> readDiscount_Offers(String path) throws IOException {
+	private List<DiscountAndOffers> discountOffersList;
 
-		List<String> discountsAndOffers = super.readFromFile(path);
-		for (String item : discountsAndOffers) {
-			String[] vendorData = item.split(Constants.COMMA);
-			Vendor vendor = new Vendor();
-			vendor.setVendorName(vendorData[0]);
-			vendor.setVendorDescription(vendorData[1]);
-			vendorList.add(vendor);
+	private Discount_OffersDataFile() {
+	}
+
+	public Discount_OffersDataFile getInstance() {
+		if (discount_OffersDataFile == null) {
+			discount_OffersDataFile = new Discount_OffersDataFile();
+		}
+		return discount_OffersDataFile;
+	}
+
+	public List<DiscountAndOffers> getDiscountOffersList() {
+		return discountOffersList;
+	}
+
+	public void setDiscountOffersList(List<DiscountAndOffers> discountOffersList) {
+		if(discountOffersList==null){
+			discountOffersList=new ArrayList<DiscountAndOffers>();
+		}
+		this.discountOffersList = discountOffersList;
+	}
+
+	public List<DiscountAndOffers> readDiscount_Offers(String path)
+			throws IOException {
+		List<String> readDiscountList = super.readFromFile(path);
+		for (String item : readDiscountList) {
+			String[] discountData = item.split(Constants.COMMA);
+			DiscountAndOffers discountAndOffers = new DiscountAndOffers();
+			discountAndOffers.setDiscountCode(discountData[0]);
+			discountAndOffers.setDescription(discountData[1]);
+			discountAndOffers.setStartDate(discountData[2]);
+			discountAndOffers.setPeriodOfDiscount(discountData[3]);
+			discountAndOffers.setPercentageDiscount(discountData[4]);
+			discountAndOffers.setIsApplicableToMemberOnly(discountData[5]);
+			discountOffersList.add(discountAndOffers);
 
 		}
-
 		return discountOffersList;
 	}
 
 	public void writeVendors(String path) throws IOException {
 		List<String> items = new ArrayList<>();
-		StringBuilder buildVendor = new StringBuilder();
-		for (Vendor vendor : vendorList) {
-			buildVendor.append(vendor.getVendorName());
-			buildVendor.append(Constants.COMMA);
-			buildVendor.append(vendor.getVendorDescription());
-			buildVendor.append(Constants.NEWLINE);
-			items.add(buildVendor.toString());
+
+		for (DiscountAndOffers discountOffers : discountOffersList) {
+			StringBuilder buildDiscount = new StringBuilder();
+			buildDiscount.append(discountOffers.getDiscountCode());
+			buildDiscount.append(Constants.COMMA);
+			buildDiscount.append(discountOffers.getDescription());
+			buildDiscount.append(Constants.COMMA);
+			buildDiscount.append(discountOffers.getStartDate());
+			buildDiscount.append(Constants.COMMA);
+			buildDiscount.append(discountOffers.getPeriodOfDiscount());
+			buildDiscount.append(Constants.COMMA);
+			buildDiscount.append(discountOffers.getPercentageDiscount());
+			buildDiscount.append(Constants.COMMA);
+			buildDiscount.append(discountOffers.getIsApplicableToMemberOnly());
+			buildDiscount.append(Constants.NEWLINE);
+			items.add(buildDiscount.toString());
 		}
 		super.writeToFile(path, items);
 	}
